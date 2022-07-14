91020021efffe3aae9cea74cdb0df144aa190c6b
Kailash <dhunganak1920@gmail.com>
2022-07-14 11:13:57 +0545

    nv

---
+++ oop.py
@@ -0,0 +1,4 @@
+class Vehicle:
+    def __init__(self,max_speed,tyre):
+        self.max__speed = max_speed
+        self.tyre = tyre
---
+++ student_class.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,name,roll):
+        self.name = name
+        self.roll = roll
+
+kailash = Student("Kailash",12)
+suren = Student("suren",15)
+anupam = Student("anupam",14)
+print(kailash.name,kailash.roll)
+print(suren.name,suren.roll)
+print(anupam.name,anupam.roll)
